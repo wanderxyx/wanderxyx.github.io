0069ed4d0fb466887a47008ceec486b9ccdcaf66
xyx <xyx>
2018-11-22 16:56:38 +0800

    test

---
+++ precache-manifest.d96c5befeafd84768972c0724f03d6af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c8b0881ca96aeb2e75af",
+    "url": "/static/js/main.c8b0881c.chunk.js"
+  },
+  {
+    "revision": "a9cd8704fb747eb0a709",
+    "url": "/static/js/1.a9cd8704.chunk.js"
+  },
+  {
+    "revision": "c8b0881ca96aeb2e75af",
+    "url": "/static/css/main.5df91b3c.chunk.css"
+  },
+  {
+    "revision": "73d0782f61d674b2b5587d659a48f248",
+    "url": "/index.html"
+  }
+];
